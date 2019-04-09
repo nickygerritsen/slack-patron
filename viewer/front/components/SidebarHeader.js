8e074dccdced238cdd30a5155e3aeb62e06e769c
Nicky Gerritsen <nickygerritsen@me.com>
2019-04-09 20:04:49 +0200

    Remove importer

--- viewer/front/components/SidebarHeader.js
+++ viewer/front/components/SidebarHeader.js
@@ -1,32 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux'
-import ConfigureWindow from './ConfigureWindow';
 
 class SidebarHeader extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      showConfigureWindow: null
-    };
-  }
-  toggleConfigureWindow() {
-    this.setState({
-      showConfigureWindow: !this.state.showConfigureWindow
-    });
   }
   render() {
     return (
       <div>
         <div className="sidebar-header">
-          <div className="team-info" onClick={this.toggleConfigureWindow.bind(this)}>
+          <div className="team-info">
             <span className="team-name">{this.props.teamInfo.name}</span>
             <p className="configure-toggler"></p>
           </div>
         </div>
-        {
-          this.state.showConfigureWindow &&
-            <ConfigureWindow toggleConfigureWindow={this.toggleConfigureWindow.bind(this)} />
-        }
       </div>
     );
   }
